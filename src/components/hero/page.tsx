5aa2fe73aa94d49db1c91b06f55c0c65e1c3977f
Thomas Hammon <hammonthomas@gmail.com>
2025-02-27 20:06:52 -0600

    Added the hero component and photographs for products

---
+++ src/components/hero/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Hero(){
+    return(
+        <div>
+            <h1>Evelyn's Health Consultations</h1>
+            <h3>Bio-Meridian Testing and Herbal Products</h3>
+        </div>
+    )
+}
